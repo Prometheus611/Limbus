10595d7745bb39f0a68c4e02c0e0ec447bcbf002
Prometheus611 <horvath.zsombor.2007@gmail.com>
2026-05-29 10:25:27 +0200

    Update megoldás 7.sql

--- megoldások/megoldás 7.sql
+++ megoldások/megoldás 7.sql
@@ -1,3 +1,3 @@
--- 7 csinálj egy lekérdezést ami vissza ad minden olyan ZAYIN kategóriás abnormalityt aminek van hozzárendelt EGO-ja
-select EGO_name, Abno_name from ego, abno
-where abno.Abno_name = ego.Abno and abno.risk like "%ZAYIN%"
+-- 7 csinálj egy lekérdezést ami vissza ad minden olyan ZAYIN kategóriás abnormalityt aminek van hozzárendelt EGO-ja és az EGO státusz effektje rupture
+select EGO_name, Abno_name, sins.status from ego, abno, sins
+where abno.Abno_name = ego.Abno and sins.id = ego.Sin_id and abno.risk like "%ZAYIN%" and sins.status like "rupture"
