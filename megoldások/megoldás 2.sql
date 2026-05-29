bcf09cb2b8b4103231f7017f14a798028448d68b
Prometheus611 <horvath.zsombor.2007@gmail.com>
2026-05-29 09:26:43 +0200

    Add query for sinners born in second half of year
    Added a query to select names of sinners born in the second half of the year.

--- megoldások/megoldás 2.sql
+++ megoldások/megoldás 2.sql
@@ -1,2 +1,3 @@
+-- 2 csinálj egy lekérdezést ami vissza adja a nevét minden sinnernek aki az év második felében született
 SELECT Sinner_name FROM sinners
 WHERE Bday LIKE "%July%" OR Bday LIKE "%August%" OR Bday LIKE "%September%" OR Bday LIKE "%October%" OR Bday LIKE "%November%" OR Bday LIKE "%December%";
