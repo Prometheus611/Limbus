1db0ddb723dc49b32a99164ca9434d7a329a421d
Prometheus611 <horvath.zsombor.2007@gmail.com>
2026-05-29 09:32:44 +0200

    Create megoldás 4.sql

---
+++ megoldások/megoldás 4.sql
@@ -0,0 +1,3 @@
+-- 4 csinálj egy lekérdezést ami vissza ad minden olyan EGO-t aminek az affinítása "lust"
+select EGO_name, Sin_name from ego, sins
+where sins.id = ego.Sin_id and sins. Sin_name like "lust"
