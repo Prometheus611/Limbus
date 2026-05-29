07efb7d7dda1d233dd974e64373671c4d26f5d78
Prometheus611 <horvath.zsombor.2007@gmail.com>
2026-05-29 09:26:23 +0200

    Add query for sinners born in February
    Add a query to retrieve names and birthdays of sinners born in February.

--- megoldások/megoldás 1.sql
+++ megoldások/megoldás 1.sql
@@ -1,2 +1,3 @@
+-- 1 cinálj egy lekérdezést ami vissza adja a nevét és születés napját minden sinner-nek aki februárban született
 select Sinner_name, Bday from sinners
 where Bday like "%February%";
